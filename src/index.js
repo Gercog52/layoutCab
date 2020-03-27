7759b968d57d086ad7f6b653394741a087ff8ea7
Gercog <54477063+Gercog52@users.noreply.github.com>
2020-03-27 16:03:09 +0800

    final layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import './js/helpers';
 import './css/css.css';
 import './sass/sass.sass';
 import './sass/media.sass';
 
-console.log('da')
+const menu = document.body.querySelector('.menu')
+
+document.body.querySelector('.menu-btn').addEventListener('click',() => {
+  menu.classList.add('menu_open')  
+})
+document.body.querySelector('.menu__control').addEventListener('click',() => {
+  menu.classList.remove('menu_open')
+})
--- src/js/helpers.js
+++
@@ -1,6 +0,0 @@
-const func = (d,b) => {
-    return d+b
-}
-const d = 10;
-console.log(d+20)
-console.log(func(10,69));
